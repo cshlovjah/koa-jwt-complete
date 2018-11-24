9d8d0837dba7eec22c0672cd6fa71bbbf8b8444f
Dmitry Zyryanov <cshlovjah@gmail.com>
2018-11-24 20:18:19 +0500

    Service work!

--- src/user/index.js
+++ src/user/index.js
@@ -1,9 +1,8 @@
 import Joi from "joi";
 import schema from "./schema";
-import redis from '../redis'
-
+import redis from "../redis";
+import Token from "../token";
 async function isAuthorized(request) {
-
   const { error, value } = Joi.validate(request, schema);
 
   if (error) return false;
@@ -15,4 +14,9 @@ async function isAuthorized(request) {
   return providedPassword == correctPassword;
 }
 
-export default { isAuthorized };
+async function hasValidRefreshToken(token) {
+  const { username } = await Token.getPayload(token);
+  const correctRefreshToken = await redis.getAsync(`${username}_refresh_token`);
+  return correctRefreshToken == token
+}
+export default { isAuthorized, hasValidRefreshToken };
