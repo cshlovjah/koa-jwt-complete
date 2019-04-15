62db325248e72259ec175ce05b7015c17437a46e
Dmitry Zyryanov <cshlovjah@gmail.com>
2019-04-15 08:46:06 +0500

    Фиксил CreateUser.js - сделал функцию которая возвращает новый обьект

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -7,17 +7,13 @@ const saltRounds = 10;
 async function CreateUser(userCredentials){
     const password = bcrypt.hashSync(userCredentials.password, saltRounds);
     const tokens = await Token.generatePair(userCredentials.username);
-    
-    const user = {
-        username: userCredentials.username,
+    const modifyUser = user => ({
+        ...user,
         password: password,
         tokens: tokens
-    }
-
-    await redis.setAsync(`${userCredentials.username}`, JSON.stringify(user));
-
-    return tokens
-    
+    })
+    await redis.setAsync(`${userCredentials.username}`, JSON.stringify(modifyUser(userCredentials)));
+    return token
 }
 
 export default CreateUser
